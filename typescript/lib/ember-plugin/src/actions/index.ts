4ea3f92b79db977604ff4bcb80f077496d3b8173
Juan Sebastián Cárdenas Rodríguez <31258166+juanscr@users.noreply.github.com>
2025-06-28 17:05:07 -0500

    Add basic token factory with swap support.

---
+++ typescript/lib/ember-plugin/src/actions/index.ts
@@ -0,0 +1,1 @@
+export type { Action, ActionDefinition } from './types.js';
---
+++ typescript/lib/ember-plugin/src/actions/types.ts
@@ -0,0 +1,37 @@
+import type { Token } from 'ember-schemas';
+import type { SwapActionCallback } from './swap.js';
+
+/**
+ * The possible actions an ember plugin can perform.
+ */
+export type Action = 'swap' | 'borrow' | 'repay' | 'supply' | 'withdraw';
+
+/**
+ * Type mapping for action callbacks.
+ */
+export type ActionCallback<T extends Action> = T extends 'swap' ? SwapActionCallback : never;
+
+/**
+ * Definition of an action that can be performed by the Ember plugin.
+ */
+export interface ActionDefinition<T extends Action> {
+  /**
+   * The action type
+   */
+  type: T;
+  /**
+   * The callback function to execute when the action is triggered.
+   */
+  callback: ActionCallback<T>;
+  /**
+   * @returns A promise that resolves to a list of tokens that can be used as input for the action.
+   */
+  inputTokens: () => Promise<Token[]>;
+  /**
+   * The function that returns the possible output tokens after the action is executed for a single input token.
+   * If this is not provided, all input tokens are assumed to produce the same output tokens.
+   * @param token One input token of the action.
+   * @returns The possible output tokens after the action is executed for that token.
+   */
+  outputTokens?: (token: Token) => Promise<Token[]>;
+}
