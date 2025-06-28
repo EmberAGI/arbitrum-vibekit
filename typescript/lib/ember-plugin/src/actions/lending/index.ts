db7d2b44365f292c884f2170673444fef1e537f3
Juan Sebastián Cárdenas Rodríguez <31258166+juanscr@users.noreply.github.com>
2025-06-28 17:19:13 -0500

    Addedn supply action types and interfaces.

--- typescript/lib/ember-plugin/src/actions/lending/index.ts
+++ typescript/lib/ember-plugin/src/actions/lending/index.ts
@@ -1,2 +1,3 @@
 export type { BorrowCallback, BorrowTokenRequest } from './borrow.js';
 export type { RepayTokensCallback, RepayTokensRequest } from './repay.js';
+export type { SupplyCallback, SupplyRequest } from './supply.js';
