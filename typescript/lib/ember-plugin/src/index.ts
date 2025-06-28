138decbb0bff257557a7b7316c4e692446148542
Juan Sebastián Cárdenas Rodríguez <31258166+juanscr@users.noreply.github.com>
2025-06-28 17:10:55 -0500

    Add borrow action.

--- typescript/lib/ember-plugin/src/index.ts
+++ typescript/lib/ember-plugin/src/index.ts
@@ -3,3 +3,7 @@ export { EmberPluginFactory } from './plugin.js';
 // Swap action types and interfaces
 export type { SwapActionRequest } from './actions/swap.js';
 export type { SwapResponse } from 'ember-schemas';
+
+// Lending action types and interfaces
+export type { BorrowCallback, BorrowTokenRequest } from './actions/lending/index.js';
+export type { BorrowResponse } from 'ember-schemas';
