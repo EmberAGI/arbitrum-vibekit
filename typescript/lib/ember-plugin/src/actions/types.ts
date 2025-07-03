81e17707f4b8809187b726e2dffcc733e14b1648
Juan Sebastián Cárdenas Rodríguez <31258166+juanscr@users.noreply.github.com>
2025-07-02 21:38:13 -0500

    Explicit definition of token sets.

--- typescript/lib/ember-plugin/src/actions/types.ts
+++ typescript/lib/ember-plugin/src/actions/types.ts
@@ -7,6 +7,7 @@ import type {
   WithdrawCallback as LendingWithdrawCallback,
 } from './lending.js';
 import type { SupplyLiquidityCallback, WithdrawLiquidityCallback } from './liquidity.js';
+import type { Chain } from 'src/common.js';
 
 /**
  * The possible actions an ember plugin can perform.
@@ -40,6 +41,25 @@ export type ActionCallback<T extends Action> = T extends 'swap'
               : never;
 
 /**
+ * Represents a grouping of tokens associated with a specific chain.
+ */
+export interface TokenSet {
+  /**
+   * An optional unique identifier for the token set.
+   * This can be used to indicate equality between input and output token sets.
+   */
+  id?: string;
+  /**
+   * The chain to which the tokens belong.
+   */
+  chain: Chain;
+  /**
+   * The set of tokens associated with the chain.
+   */
+  tokens: Set<Token>;
+}
+
+/**
  * Definition of an action that can be performed by the Ember plugin.
  */
 export interface ActionDefinition<T extends Action> {
@@ -52,14 +72,14 @@ export interface ActionDefinition<T extends Action> {
    */
   callback: ActionCallback<T>;
   /**
-   * @returns A promise that resolves to a list of tokens that can be used as input for the action.
+   * THis function returns the possible input tokens for the action in all chains.
+   * @returns The list of token sets that can be used as input for the action.
    */
-  inputTokens: () => Promise<Token[]>;
+  inputTokens: () => Promise<TokenSet[]>;
   /**
-   * The function that returns the possible output tokens after the action is executed for a single input token.
-   * If this is not provided, all input tokens are assumed to produce the same output tokens.
-   * @param token One input token of the action.
-   * @returns The possible output tokens after the action is executed for that token.
+   * This function returns the possible output tokens for the action in all chains.
+   * If not provided, all input input token sets will be considered possible output sets.
+   * @returns The list of tokens that can be used as output for the action.
    */
-  outputTokens?: (token: Token) => Promise<Token[]>;
+  outputTokens?: () => Promise<TokenSet[]>;
 }
