f6b7f2b929b62f44effa78903d7f2ded4b0bb5b1
Juan Sebastián Cárdenas Rodríguez <31258166+juanscr@users.noreply.github.com>
2025-09-11 13:15:19 -0500

    feat(): Receive chain config instead of aave related constructor.

--- typescript/plugins/registry/src/index.ts
+++ typescript/plugins/registry/src/index.ts
@@ -1,3 +1,4 @@
+import type { ChainConfig } from './chainConfig.js';
 import { PublicEmberPluginRegistry } from './registry.js';
 import { getAaveEmberPlugin, type AAVEAdapterParams } from '@ember/aave-lending-plugin';
 
@@ -5,11 +6,20 @@ import { getAaveEmberPlugin, type AAVEAdapterParams } from '@ember/aave-lending-
  * Initialize the public Ember plugin registry.
  * @returns The initialized public Ember plugin registry with registered plugins.
  */
-export function initializePublicRegistry(aaveParams: AAVEAdapterParams) {
+export function initializePublicRegistry(chainConfigs: ChainConfig[]) {
   const registry = new PublicEmberPluginRegistry();
 
   // Register any plugin in here
-  registry.registerDeferredPlugin(getAaveEmberPlugin(aaveParams));
+  for (const chainConfig of chainConfigs) {
+    // Create aave plugins for each chain config
+    registry.registerDeferredPlugin(
+      getAaveEmberPlugin({
+        chainId: chainConfig.chainId,
+        rpcUrl: chainConfig.rpcUrl,
+        wrappedNativeToken: chainConfig.wrappedNativeToken,
+      })
+    );
+  }
 
   return registry;
 }
