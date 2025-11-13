9334887fb4babb61b58a44a65dffdeaa164b7798
ParastooGM <parastoogmohammadi@yahoo.com>
2025-11-13 13:46:08 -0500

    delete extra files

--- typescript/test-example.ts
+++
@@ -1,9 +0,0 @@
-// Test file for documentation
-export function testFunction() {
-  return 'hello world';
-}
-
-// New function added
-export function newTestFunction() {
-  return 'new functionality';
-}
