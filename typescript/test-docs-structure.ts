ba2df42b4b886932a334d83e93618e97119241cf
ParastooGM <parastoogmohammadi@yahoo.com>
2025-11-13 11:57:33 -0500

    Add test file for new docs structure

---
+++ typescript/test-docs-structure.ts
@@ -0,0 +1,9 @@
+// Test file for new docs structure
+export interface TestInterface {
+  id: string;
+  name: string;
+}
+
+export function testFunction(): string {
+  return 'testing new docs structure';
+}
