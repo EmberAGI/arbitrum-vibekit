b5b80740d56528bb23692d837e13278d174fb2a2
Sebas <sebas@MacBookPro.lan>
2026-02-06 11:14:55 -0800

    fix(web): allow firing agent while setup interrupt is pending

--- typescript/clients/web-ag-ui/apps/web/src/hooks/useAgentConnection.ts
+++ typescript/clients/web-ag-ui/apps/web/src/hooks/useAgentConnection.ts
@@ -38,6 +38,7 @@ import { cleanupAgentConnection } from '../utils/agentConnectionCleanup';
 import { fireAgentRun } from '../utils/fireAgentRun';
 import { scheduleCycleAfterInterruptResolution } from '../utils/interruptAutoCycle';
 import { cleanupAgentConnection } from '../utils/agentConnectionCleanup';
+import { fireAgentRun } from '../utils/fireAgentRun';
 
 export type {
   AgentState,
